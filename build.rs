f304a5f64ff43a7dab952cbae6628eb8c19b1050
toni-zana <tonizana.dev@gmail.com>
2024-08-29 17:57:28 +0200

    fix: force disable ipo in pocketpy to fix linker errors

--- build.rs
+++ build.rs
@@ -20,6 +20,20 @@ fn main() {
 		.write_to_file(out_path.join("bindings.rs"))
 		.expect("failed to write generated bindings");
 
+	// We have to force disable IPO/LTO otherwise linker will crash
+	const CMAKE_LISTS_PATH: &str = "./vendor/pocketpy/CMakeLists.txt";
+
+	let original_cmake_lists_contents = std::fs::read_to_string(CMAKE_LISTS_PATH).unwrap();
+
+	std::fs::write(
+		CMAKE_LISTS_PATH,
+		original_cmake_lists_contents.replace(
+			"set(CMAKE_INTERPROCEDURAL_OPTIMIZATION TRUE)",
+			"set(CMAKE_INTERPROCEDURAL_OPTIMIZATION FALSE)",
+		),
+	)
+	.unwrap();
+
 	// Build pocketpy as static lib
 	let built = cmake::Config::new("vendor/pocketpy")
 		.define("PK_BUILD_SHARED_LIB", "OFF")
@@ -36,4 +50,7 @@ fn main() {
 	}
 
 	println!("cargo:rustc-link-lib=static=pocketpy");
+
+	// Restore the original CMakeLists.txt
+	std::fs::write(CMAKE_LISTS_PATH, original_cmake_lists_contents).unwrap();
 }
